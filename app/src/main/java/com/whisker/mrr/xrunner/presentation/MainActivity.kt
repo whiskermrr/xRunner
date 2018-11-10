7fefe258db2dfe2880fb7633d4575d744d2b59c7
Wiktor <witek119@gmail.com>
2018-11-10 01:27:51 +0100

    added fade in/out fragment transaction

--- app/src/main/java/com/whisker/mrr/xrunner/presentation/MainActivity.kt
+++ app/src/main/java/com/whisker/mrr/xrunner/presentation/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
 import com.google.firebase.auth.FirebaseAuth
 import com.whisker.mrr.xrunner.R
 import com.whisker.mrr.xrunner.di.Injectable
@@ -73,7 +74,7 @@ class MainActivity : AppCompatActivity(), Injectable, HasSupportFragmentInjector
         val manager = supportFragmentManager
         val previousFragment = getTopFragment(manager)
         val ft = manager.beginTransaction()
-        ft.setTransition(androidx.fragment.app.FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+        ft.setTransition(androidx.fragment.app.FragmentTransaction.TRANSIT_FRAGMENT_FADE)
         if (previousFragment != null) {
             ft.hide(previousFragment)
         }
