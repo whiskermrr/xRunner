698e974e2d39669744458e2d1b93635a80ab16df
Wiktor <witek119@gmail.com>
2018-11-18 16:07:20 +0100

    working passing and saving to db in summary fragment

--- app/src/main/java/com/whisker/mrr/xrunner/presentation/summary/SummaryRunFragment.kt
+++ app/src/main/java/com/whisker/mrr/xrunner/presentation/summary/SummaryRunFragment.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.whisker.mrr.xrunner.R
 import com.whisker.mrr.xrunner.domain.model.Route
@@ -14,6 +16,10 @@ class SummaryRunFragment : BaseFragment() {
 
     private lateinit var viewModel: SummaryRunViewModel
     private lateinit var finalRoute: Route
+
+    private val routeSavedObserver = Observer<Boolean> {
+        Toast.makeText(mainActivity, "Route Saved", Toast.LENGTH_SHORT).show()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,5 +37,9 @@ class SummaryRunFragment : BaseFragment() {
         viewModel = mainActivity.run {
             ViewModelProviders.of(this, viewModelFactory).get(SummaryRunViewModel::class.java)
         }
+
+        viewModel.getIsRouteSaved().observe(this, routeSavedObserver)
+
+        viewModel.saveRoute(finalRoute)
     }
 }
--- app/src/main/java/com/whisker/mrr/xrunner/presentation/summary/SummaryRunViewModel.kt
+++ app/src/main/java/com/whisker/mrr/xrunner/presentation/summary/SummaryRunViewModel.kt
@@ -25,4 +25,6 @@ class SummaryRunViewModel
                 }
         )
     }
+
+    fun getIsRouteSaved() = isRouteSaved
 }
