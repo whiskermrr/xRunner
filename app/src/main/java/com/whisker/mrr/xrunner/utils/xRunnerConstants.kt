845da54ea392d505837e9ba71a2bed854e18c620
whiskermrr <witek119@gmail.com>
2018-12-10 22:30:58 +0100

    removed unused constants

--- app/src/main/java/com/whisker/mrr/xrunner/utils/xRunnerConstants.kt
+++ app/src/main/java/com/whisker/mrr/xrunner/utils/xRunnerConstants.kt
@@ -2,11 +2,6 @@ package com.whisker.mrr.xrunner.utils
 
 object xRunnerConstants {
 
-    const val MILLISECONDS_PER_HOUR = 3600000
-    const val MILLISECONDS_PER_MINUTE = 60000
-    const val MILLISECONDS_PER_SECOND = 1000
-    const val SECONDS_PER_HOUR = 3600
-
     const val REQUEST_LOCATION_CODE = 99
 
     const val EXTRA_FINAL_ROUTE_KEY = "finalRoute"
