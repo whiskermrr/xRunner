dc9e1a36a3d961eb51006b548c9b40a429307a48
whiskermrr <witek119@gmail.com>
2018-12-10 23:46:48 +0100

    removed log from extension func

--- app/src/main/java/com/whisker/mrr/xrunner/utils/Extensions.kt
+++ app/src/main/java/com/whisker/mrr/xrunner/utils/Extensions.kt
@@ -1,6 +1,5 @@
 package com.whisker.mrr.xrunner.utils
 
-import android.util.Log
 import android.widget.ImageView
 import com.google.firebase.storage.FirebaseStorage
 import com.squareup.picasso.Picasso
@@ -9,7 +8,6 @@ fun Any.TAG() = this::class.java.simpleName
 
 fun ImageView.loadSnapshot(fileName: String) {
     if(FileUtils.fileExists(context, fileName)) {
-        Log.e("IMAGE VIEW", "File Exists")
         val image = FileUtils.getFile(context, fileName)
         Picasso.get().load(image).into(this)
     } else {
