7f0f14cfdc1578d8fe0734ebc4e52afb96dcc0f9
whiskermrr <witek119@gmail.com>
2018-12-13 12:56:08 +0100

    added more extension functions and removed utils

--- app/src/main/java/com/whisker/mrr/xrunner/utils/Extensions.kt
+++ app/src/main/java/com/whisker/mrr/xrunner/utils/Extensions.kt
@@ -1,14 +1,19 @@
 package com.whisker.mrr.xrunner.utils
 
+import android.content.Context
+import android.graphics.Bitmap
 import android.widget.ImageView
 import com.google.firebase.storage.FirebaseStorage
 import com.squareup.picasso.Picasso
+import java.io.ByteArrayOutputStream
+import java.io.File
+import java.io.FileOutputStream
 
-fun Any.TAG() = this::class.java.simpleName
+fun Any.TAG() = javaClass.simpleName
 
 fun ImageView.loadSnapshot(fileName: String) {
-    if(FileUtils.fileExists(context, fileName)) {
-        val image = FileUtils.getFile(context, fileName)
+    if(context.fileExists(fileName)) {
+        val image = context.getFile(fileName)
         Picasso.get().load(image).into(this)
     } else {
         val imageReference = FirebaseStorage.getInstance().reference.child("snapshots/$fileName")
@@ -16,5 +21,26 @@ fun ImageView.loadSnapshot(fileName: String) {
             Picasso.get().load(it).into(this)
         }
     }
+}
 
+fun Bitmap.toByteArray(format: Bitmap.CompressFormat) : ByteArray {
+    val byteArray = ByteArrayOutputStream()
+    this.compress(format, 100, byteArray)
+    return byteArray.toByteArray()
+}
+
+fun Context.saveFile(data: ByteArray?, fileName: String) {
+    val outputStream: FileOutputStream = openFileOutput(fileName, Context.MODE_PRIVATE)
+    outputStream.write(data)
+    outputStream.flush()
+    outputStream.close()
+}
+fun Context.getFile(fileName: String) : File {
+    val filePath = getFileStreamPath(fileName).absolutePath
+    return File(filePath)
+}
+
+fun Context.fileExists(fileName: String) : Boolean {
+    val file = getFileStreamPath(fileName)
+    return file.exists()
 }
