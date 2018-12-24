5f56746ffb1375949f3fc3b3f571d58ff36f4c1c
whiskermrr <witek119@gmail.com>
2018-12-24 17:53:15 +0100

    added swiping delete to view

--- app/src/main/java/com/whisker/mrr/xrunner/presentation/history/PastRoutesFragment.kt
+++ app/src/main/java/com/whisker/mrr/xrunner/presentation/history/PastRoutesFragment.kt
@@ -6,10 +6,13 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.whisker.mrr.xrunner.R
 import com.whisker.mrr.xrunner.presentation.BaseFragment
 import com.whisker.mrr.xrunner.presentation.adapters.RoutesAdapter
+import com.whisker.mrr.xrunner.presentation.common.SwipeToDeleteCallback
 import io.github.luizgrp.sectionedrecyclerviewadapter.SectionedRecyclerViewAdapter
 import kotlinx.android.synthetic.main.fragment_past_routes.*
 
@@ -29,6 +32,22 @@ class PastRoutesFragment : BaseFragment() {
         routesAdapter = SectionedRecyclerViewAdapter()
         rvRoutes.layoutManager = LinearLayoutManager(context)
         rvRoutes.adapter = routesAdapter
+
+        val swipeHandler = object : SwipeToDeleteCallback(context!!) {
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                val section = routesAdapter.getSectionForPosition(viewHolder.adapterPosition) as RoutesAdapter
+                val globalPosition = routesAdapter.getPositionInAdapter(section, 0)
+                val route = section.removeItem(viewHolder.adapterPosition - globalPosition)
+                routesAdapter.notifyItemRemovedFromSection(section, viewHolder.adapterPosition - globalPosition)
+                if(section.contentItemsTotal == 0) {
+                    routesAdapter.removeSection(section)
+                    routesAdapter.notifyDataSetChanged()
+                }
+            }
+        }
+
+        val itemTouchHelper = ItemTouchHelper(swipeHandler)
+        itemTouchHelper.attachToRecyclerView(rvRoutes)
 
         viewModel.getRouteList().observe(this, Observer {holders ->
             routesAdapter.removeAllSections()
