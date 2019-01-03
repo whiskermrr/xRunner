e20b3d49ae62a03df31c3d1632407d1a7b514c7c
whiskermrr <witek119@gmail.com>
2019-01-03 06:15:56 +0100

    added more stats to user profile
    -also changed some icons sizes

--- app/src/main/java/com/whisker/mrr/xrunner/presentation/views/profile/UserProfileFragment.kt
+++ app/src/main/java/com/whisker/mrr/xrunner/presentation/views/profile/UserProfileFragment.kt
@@ -1,6 +1,7 @@
 package com.whisker.mrr.xrunner.presentation.views.profile
 
 import android.animation.ObjectAnimator
+import android.animation.ValueAnimator
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -25,11 +26,25 @@ class UserProfileFragment : BaseFragment() {
         viewModel = ViewModelProviders.of(this, viewModelFactory).get(UserProfileViewModel::class.java)
 
         viewModel.userStats().observe(this, Observer { stats ->
-            tvLevel.text = stats.level.toString()
-            val progressAnimation = ObjectAnimator.ofInt(expBar, "progress", 0, stats.percentExp)
-            progressAnimation.duration = 500
-            progressAnimation.interpolator = DecelerateInterpolator()
-            progressAnimation.start()
+            showLevelNumber(stats.level)
+            showLevelProgress(stats.percentExp)
         })
+    }
+
+    private fun showLevelNumber(level: Int) {
+        val animator = ValueAnimator()
+        animator.setObjectValues(0, level)
+        animator.duration = 700
+        animator.addUpdateListener { animation ->
+            tvLevel.text = animation.animatedValue.toString()
+        }
+        animator.start()
+    }
+
+    private fun showLevelProgress(percentExp: Int) {
+        val progressAnimation = ObjectAnimator.ofInt(expBar, "progress", 0, percentExp)
+        progressAnimation.duration = 700
+        progressAnimation.interpolator = DecelerateInterpolator()
+        progressAnimation.start()
     }
 }
