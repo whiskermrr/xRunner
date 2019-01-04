6d272cb019d9f24ac6cefe88c1a7772889adcf47
whiskermrr <witek119@gmail.com>
2019-01-04 10:51:54 +0100

    added data class achievement

---
+++ app/src/main/java/com/whisker/mrr/xrunner/domain/model/Achievement.kt
@@ -0,0 +1,13 @@
+package com.whisker.mrr.xrunner.domain.model
+
+data class Achievement(
+    var isFinished: Boolean = false,
+    var deadline: Long? = null,
+    var title: String = "Achievement",
+    var distance: Int? = null,
+    var speed: Float? = null,
+    var time: Long? = null,
+    var progress: Int = 0,
+    var difficulty: Int = 0,
+    var experience: Int = 0
+)
