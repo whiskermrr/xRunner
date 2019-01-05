4d72a9a0852b86384e4a4d2a3b1541bca1d44982
whiskermrr <witek119@gmail.com>
2019-01-05 06:24:36 +0100

    almost done achievements repo

--- app/src/main/java/com/whisker/mrr/xrunner/data/repository/AchievementsDataRepository.kt
+++ app/src/main/java/com/whisker/mrr/xrunner/data/repository/AchievementsDataRepository.kt
@@ -11,57 +11,90 @@ import com.whisker.mrr.xrunner.domain.model.RouteStatsEntity
 import com.whisker.mrr.xrunner.domain.repository.AchievementsRepository
 import io.reactivex.Completable
 import io.reactivex.Single
+import java.util.*
 
 class AchievementsDataRepository(private val databaseReference: DatabaseReference) : AchievementsRepository {
 
     override fun saveAchievement(userId: String, achievement: Achievement): Completable {
-        val reference = databaseReference
-            .child(REFERENCE_USERS)
-            .child(userId)
-            .child(REFERENCE_ACHIEVEMENTS)
-
+        val reference = getReference(userId)
         achievement.id = reference.push().key!!
 
         return Completable.create { emitter ->
             reference.child(achievement.id).setValue(achievement).addOnCompleteListener { task ->
                 if(task.isSuccessful) {
                     emitter.onComplete()
-                } else if(task.exception != null) {
-                    emitter.onError(task.exception!!)
+                } else {
+                    task.exception?.let {
+                        emitter.onError(it)
+                    }
                 }
             }
         }
     }
 
     override fun updateAchievements(userId: String, stats: RouteStatsEntity): Completable {
-        val reference = databaseReference
-            .child(REFERENCE_USERS)
-            .child(userId)
-            .child(REFERENCE_ACHIEVEMENTS)
-            .orderByValue()
+        val reference = getReference(userId)
+        return getActiveAchievements(reference)
+            .flatMapCompletable {
+                Completable.create { emitter ->
+
+                }
+            }
     }
 
     override fun getAchievements(userId: String): Single<List<Achievement>> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return Single.create { emitter ->
+            getReference(userId).addListenerForSingleValueEvent(object: ValueEventListener {
+                override fun onDataChange(dataSnapshot: DataSnapshot) {
+                    val achievements = mutableListOf<Achievement>()
+                    dataSnapshot.children.forEach { child ->
+                        child.getValue(Achievement::class.java)?.let {
+                            achievements.add(it)
+                        }
+                    }
+                    emitter.onSuccess(achievements)
+                }
+
+                override fun onCancelled(databaseError: DatabaseError) {
+                    emitter.onError(databaseError.toException())
+                }
+            })
+        }
     }
 
     override fun getActiveAchievements(userId: String) : Single<List<Achievement>> {
-
+        return getActiveAchievements(getReference(userId))
     }
 
     private fun getActiveAchievements(reference: DatabaseReference) : Single<List<Achievement>> {
         return Single.create { emitter ->
             reference.addListenerForSingleValueEvent(object: ValueEventListener {
                 override fun onDataChange(dataSnapshot: DataSnapshot) {
-                    if(dataSnapshot.exists()) {
-
+                    val currentTime = Date().time
+                    val activeAchievements = mutableListOf<Achievement>()
+                    dataSnapshot.children.forEach { child ->
+                        child.getValue(Achievement::class.java)?.let { achievement ->
+                            achievement.deadline?.let {
+                                if(it > currentTime) activeAchievements.add(achievement)
+                            } ?: achievement.run {
+                                activeAchievements.add(this)
+                            }
+                        }
                     }
+                    emitter.onSuccess(activeAchievements)
                 }
 
-                override fun onCancelled(dataSnapshot: DatabaseError) {
-                    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+                override fun onCancelled(databaseError: DatabaseError) {
+                    emitter.onError(databaseError.toException())
                 }
             })
         }
+    }
+
+    private fun getReference(userId: String) : DatabaseReference {
+        return databaseReference
+            .child(REFERENCE_USERS)
+            .child(userId)
+            .child(REFERENCE_ACHIEVEMENTS)
     }
 }
