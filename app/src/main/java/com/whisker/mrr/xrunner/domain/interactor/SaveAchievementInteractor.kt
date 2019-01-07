6c7e51f51997b10aa6ac04887b6445d523ddbba3
whiskermrr <witek119@gmail.com>
2019-01-07 06:38:37 +0100

    added save and update Interactors

---
+++ app/src/main/java/com/whisker/mrr/xrunner/domain/interactor/SaveAchievementInteractor.kt
@@ -0,0 +1,37 @@
+package com.whisker.mrr.xrunner.domain.interactor
+
+import com.whisker.mrr.xrunner.domain.model.Achievement
+import com.whisker.mrr.xrunner.domain.repository.AchievementsRepository
+import com.whisker.mrr.xrunner.domain.source.AuthSource
+import com.whisker.mrr.xrunner.domain.usecase.CompletableUseCase
+import io.reactivex.Completable
+import io.reactivex.CompletableTransformer
+import java.lang.IllegalArgumentException
+
+class SaveAchievementInteractor(
+    transformer: CompletableTransformer,
+    private val authSource: AuthSource,
+    private val achievementsRepository: AchievementsRepository
+) : CompletableUseCase(transformer) {
+
+    companion object {
+        const val PARAM_ACHIEVEMENT = "param_achievement"
+    }
+
+    fun saveAchievement(achievement: Achievement) : Completable {
+        val data = HashMap<String, Any>()
+        data[PARAM_ACHIEVEMENT] = achievement
+        return completable(data)
+    }
+
+    override fun createCompletable(data: Map<String, Any>?): Completable {
+        val achievement = data?.get(PARAM_ACHIEVEMENT)
+
+        achievement?.let {
+            return authSource.getUserId()
+                .flatMapCompletable {  userId ->
+                    achievementsRepository.saveAchievement(userId, achievement as Achievement)
+                }
+        } ?: return Completable.error(IllegalArgumentException("Argument @achievement must be provided."))
+    }
+}
