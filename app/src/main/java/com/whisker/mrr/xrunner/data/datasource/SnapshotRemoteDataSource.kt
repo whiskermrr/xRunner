f27539b87e011c934c197f7056eb685774b4feb2
whiskermrr <witek119@gmail.com>
2019-01-19 21:26:14 +0100

    changed uri to filestream

--- app/src/main/java/com/whisker/mrr/xrunner/data/datasource/SnapshotRemoteDataSource.kt
+++ app/src/main/java/com/whisker/mrr/xrunner/data/datasource/SnapshotRemoteDataSource.kt
@@ -1,19 +1,19 @@
 package com.whisker.mrr.xrunner.data.datasource
 
-import android.net.Uri
 import com.google.firebase.storage.FirebaseStorage
 import com.whisker.mrr.xrunner.data.common.DataConstants.REFERENCE_SNAPSHOT
 import com.whisker.mrr.domain.source.SnapshotRemoteSource
 import io.reactivex.Completable
 import java.io.File
+import java.io.FileInputStream
 
 class SnapshotRemoteDataSource(private val firebaseStorage: FirebaseStorage) : SnapshotRemoteSource {
 
     override fun saveSnapshotRemote(filePath: String, fileName: String) : Completable {
-        val fileUri = Uri.fromFile(File(filePath))
+        val stream = FileInputStream(File(filePath))
         val snapshotReference = firebaseStorage.reference.child(REFERENCE_SNAPSHOT + fileName)
         return Completable.create { emitter ->
-            snapshotReference.putFile(fileUri).addOnCompleteListener { task ->
+            snapshotReference.putStream(stream).addOnCompleteListener { task ->
                 if(task.isSuccessful) {
                     emitter.onComplete()
                 } else {
