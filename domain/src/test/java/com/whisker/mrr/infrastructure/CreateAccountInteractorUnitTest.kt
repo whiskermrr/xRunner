0f680fc1eaf4f29a49cbb34af39fe355ebeb9607
whiskermrr <witek119@gmail.com>
2019-02-20 19:16:31 +0100

    unit tests for create account interactor

--- domain/src/test/java/com/whisker/mrr/infrastructure/CreateAccountInteractorUnitTest.kt
+++ domain/src/test/java/com/whisker/mrr/infrastructure/CreateAccountInteractorUnitTest.kt
@@ -2,26 +2,29 @@ package com.whisker.mrr.infrastructure
 
 import com.nhaarman.mockito_kotlin.doReturn
 import com.nhaarman.mockito_kotlin.mock
-import com.nhaarman.mockito_kotlin.whenever
-import com.whisker.mrr.domain.common.scheduler.IOCompletableTransformer
 import com.whisker.mrr.domain.interactor.CreateAccountInteractor
 import com.whisker.mrr.domain.repository.LoginRepository
 import com.whisker.mrr.domain.repository.UserRepository
 import io.reactivex.Completable
 import io.reactivex.Single
-import io.reactivex.schedulers.Schedulers
+import org.junit.Before
 import org.junit.Test
 import org.mockito.ArgumentMatchers.anyString
+import org.mockito.MockitoAnnotations
+import java.lang.IllegalArgumentException
 
 class CreateAccountInteractorUnitTest {
 
-    private val loginRepository = mock<LoginRepository>()
-    private val userRepository = mock<UserRepository>()
+    private var loginRepository = mock<LoginRepository>()
 
-    private val transformer: IOCompletableTransformer = IOCompletableTransformer(Schedulers.trampoline())
 
-    private val createAccountInteractor by lazy {
-        CreateAccountInteractor(transformer, loginRepository, userRepository)
+    private var userRepository = mock<UserRepository>()
+
+    private var createAccountInteractor = mock<CreateAccountInteractor>()
+
+    @Before
+    fun setUp() {
+        MockitoAnnotations.initMocks(this)
     }
 
     @Test
@@ -32,8 +35,7 @@ class CreateAccountInteractorUnitTest {
         doReturn(Completable.complete()).`when`(userRepository).createUserStats(userId)
 
         val response = Completable.complete()
-        whenever(createAccountInteractor.createAccount(anyString(), anyString()))
-            .thenReturn(response)
+        doReturn(response).`when`(createAccountInteractor).createAccount(anyString(), anyString())
 
         createAccountInteractor.createAccount("test@test.com", "12345678")
             .test()
@@ -42,11 +44,36 @@ class CreateAccountInteractorUnitTest {
 
     @Test
     fun createAccountFailed() {
-        whenever(createAccountInteractor.createAccount(anyString(), anyString()))
-            .thenReturn(Completable.error(Throwable("Error")))
+        val error = Throwable("Error")
+        val singleError = Single.error<Throwable>(error)
+        val completableError = Completable.error(error)
+        doReturn(singleError).`when`(loginRepository).createAccount(anyString(), anyString())
+        doReturn(completableError).`when`(createAccountInteractor).createAccount(anyString(), anyString())
 
         createAccountInteractor.createAccount("test@test.com", "12345678")
             .test()
-            .assertError(Throwable("Error"))
+            .assertError(error)
+    }
+
+    @Test
+    fun createAccountIllegalArgumentException() {
+        val error = IllegalArgumentException("@email and @password must be provided.")
+        val completableError = Completable.error(error)
+        doReturn(completableError).`when`(createAccountInteractor).createCompletable(null)
+
+        createAccountInteractor.createCompletable(null)
+            .test()
+            .assertError(error)
+    }
+
+    @Test
+    fun createAccountIllegalArgumentExceptionTwo() {
+        val error = IllegalArgumentException("@email and @password must be provided.")
+        val completableError = Completable.error(error)
+        doReturn(completableError).`when`(createAccountInteractor).completable(null)
+
+        createAccountInteractor.completable(null)
+            .test()
+            .assertError(error)
     }
 }
