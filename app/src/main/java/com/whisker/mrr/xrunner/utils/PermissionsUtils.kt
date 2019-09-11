1762995fcfb5bd58749c9bdc6db1ced778711598
whiskermrr <witek119@gmail.com>
2019-09-11 13:43:24 +0200

    added permissions dialog

--- app/src/main/java/com/whisker/mrr/xrunner/utils/PermissionsUtils.kt
+++ app/src/main/java/com/whisker/mrr/xrunner/utils/PermissionsUtils.kt
@@ -1,8 +1,15 @@
 package com.whisker.mrr.xrunner.utils
 
+import android.app.AlertDialog
+import android.content.Context
+import android.content.DialogInterface
+import android.content.Intent
 import android.content.pm.PackageManager
+import android.net.Uri
+import android.provider.Settings
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import com.whisker.mrr.xrunner.R
 
 object PermissionsUtils {
 
@@ -43,7 +50,50 @@ object PermissionsUtils {
         return false
     }
 
-    private fun isPermissionGranted(fragment: Fragment, permission: String) : Boolean {
+    fun onRequestPermissionDenied(fragment: Fragment, permissions: Array<String>, rationaleMessage: String? = null) {
+        var isShowRationale = false
+        for(permission in permissions) {
+            if(fragment.shouldShowRequestPermissionRationale(permission)) {
+                isShowRationale = true
+                break
+            }
+        }
+
+        val message = rationaleMessage ?: R.string.permission_required_info.toString()
+
+        showPermissionDialog(fragment, permissions, message, isShowRationale)
+    }
+
+    fun showPermissionDialog(fragment: Fragment, permissions: Array<String>, rationaleMessage: String, isShowRationale: Boolean) {
+        val dialogBuilder = AlertDialog.Builder(fragment.requireContext())
+        dialogBuilder.setMessage(rationaleMessage)
+        dialogBuilder.setCancelable(true)
+
+        if(isShowRationale) {
+            dialogBuilder.setPositiveButton(R.string.button_ok) { _, _ ->
+                okAction(fragment, permissions)
+            }
+        } else {
+            dialogBuilder.setPositiveButton(R.string.button_settings) { _, _ ->
+                settingsAction(fragment)
+            }
+        }
+    }
+
+    private fun okAction(fragment: Fragment, permissions: Array<String>) {
+        if(fragment.isAdded) {
+            fragment.requestPermissions(permissions, REQUEST_CODE)
+        }
+    }
+
+    private fun settingsAction(fragment: Fragment) {
+        val settingsIntent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+        val uri = Uri.fromParts("package", fragment.requireContext().packageName, null)
+        settingsIntent.data = uri
+        fragment.startActivityForResult(settingsIntent, 8938)
+    }
+
+    fun isPermissionGranted(fragment: Fragment, permission: String) : Boolean {
         return ContextCompat.checkSelfPermission(fragment.requireContext(), permission) == PackageManager.PERMISSION_GRANTED
     }
 }
