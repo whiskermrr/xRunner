13bf40366739af5b9b828aeaad74c42d5a91998b
whiskermrr <witek119@gmail.com>
2019-09-25 18:07:54 +0200

    added own rxjava global error handler

--- app/src/main/java/com/whisker/mrr/xrunner/App.kt
+++ app/src/main/java/com/whisker/mrr/xrunner/App.kt
@@ -1,5 +1,6 @@
 package com.whisker.mrr.xrunner
 
+import android.util.Log
 import androidx.work.Configuration
 import androidx.work.WorkManager
 import com.facebook.stetho.Stetho
@@ -7,11 +8,13 @@ import com.whisker.mrr.xrunner.di.DaggerAppComponent
 import com.whisker.mrr.xrunner.di.applyAutoInjector
 import dagger.android.AndroidInjector
 import dagger.android.DaggerApplication
+import io.reactivex.plugins.RxJavaPlugins
 
 class App : DaggerApplication() {
 
     override fun onCreate() {
         super.onCreate()
+        RxJavaPlugins.setErrorHandler { Log.e("xRunner", "error after termination") }
         this.applyAutoInjector()
         Stetho.initializeWithDefaults(this)
     }
